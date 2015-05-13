e164baaeb6054c3289b703db0eb471b809e897dc
Arclet <exmercenary@hotmail.com>
2015-05-14 10:59:34 +1200

    Update results.php

--- results.php
+++ results.php
@@ -9,8 +9,8 @@
 		  <?php
 		    $name = $_POST["practice"];
 		    echo "<p> Results for $name :</p>";
-		    ?>
-		    
+		  ?>
+	<a href="mainmenu.php">Return to Home Page</a>	    
 	</body>
 </html>
 	
