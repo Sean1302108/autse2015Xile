778c2d1c1b39440eade1405794165efa2f577022
Arclet <exmercenary@hotmail.com>
2015-05-14 10:56:52 +1200

    Create results.php

---
+++ results.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Search Form</title>
+	</head>
+	<body>
+		
+		<h1>SERLER</h1>
+		  <?php
+		    $name = $_POST["practice"];
+		    echo "<p> Results for $name :</p>"
+		    ?>
+		    
+		</body>
+		</html>
+	
