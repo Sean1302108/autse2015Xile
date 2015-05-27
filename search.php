0dd04c54997d9a931b9b0ae57364c84578152edf
Sean.Y <sean_young_83@hotmail.com>
2015-05-28 10:06:24 +1200

    Create search.php

---
+++ search.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+
+<html>
+	
+	<head>
+		<title>Search Status Form</title>
+	</head>
+	<body>
+
+			
+			<form action = "postsearch.php" method = "GET" >
+				Status: 
+				<input type="text" name="input">
+				<input type="submit" value="View Status">
+			</form>
+			
+			<p>
+				<a href="form.php">Return to Home Page </a>
+			</p>
+			
+			
+</body>
+</html>
+
