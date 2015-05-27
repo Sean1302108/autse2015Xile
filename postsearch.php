0cfcbb9bb98444488578af4c3b6fef79e4e47eda
Sean.Y <sean_young_83@hotmail.com>
2015-05-28 10:23:05 +1200

    Update postsearch.php

--- postsearch.php
+++ postsearch.php
@@ -22,17 +22,32 @@
 				echo "<table width='200%' border ='1'>";
 				echo "<tr><th>methodology name</th><th>methodology description</th><th>method name</th><th>method description</th><th>bib ref</th><th>research level</th><th>Rating</th><th>cred reason</th><th>credrater</th><th>question</th><th>rd method</th><th>metrics</th><th>participants</th><th>benefit</th><th>result</th><th>method</th><th>context</th><th>con level</th><th>con reason</th><th>con rater</th></tr>";
 				
-		/* 		while($row = mysqli_fetch_assoc($results)) {
+						while($row = mysqli_fetch_assoc($results)) {
 					echo "<tr><td> {$row['methodologyname']}</td>";
 					echo "<td> {$row['methodologydescription']}</td>";
 					echo "<td> {$row['methodname']}</td>";
 					echo "<td> {$row['methoddescription']}</td>";
 					echo "<td> {$row['bibref']}</td>";
+					echo "<td> {$row['researchlevel']}</td>";
+					echo "<td> {$row['radio']}</td>";
+					echo "<td> {$row['credreason']}</td>";
+					echo "<td> {$row['credrater']}</td>";
+					echo "<td> {$row['question']}</td>";
+					echo "<td> {$row['rdmethod']}</td>";
+					echo "<td> {$row['metrics']}</td>";
+					echo "<td> {$row['participants']}</td>";
+					echo "<td> {$row['benefit']}</td>";
+					echo "<td> {$row['result']}</td>";
+					echo "<td> {$row['method']}</td>";
+					echo "<td> {$row['context']}</td>";
+					echo "<td> {$row['conlevel']}</td>";
+					echo "<td> {$row['conreason']}</td>";
+					echo "<td> {$row['conrater']}</td>";
 					echo "</tr>";
 				}
-				
+			
 				mysqli_free_result($results);
-				mysqli_close($conn); */
+				mysqli_close($conn);
 			?>
 
 </body>
