2b9cc51ba9183852fb4e629647a531ef94b344cb
Sean.Y <sean_young_83@hotmail.com>
2015-05-29 10:28:17 +1200

    Update postsearch.php

--- postsearch.php
+++ postsearch.php
@@ -16,13 +16,13 @@
 				@mysqli_select_db($conn, $dbnm)
 				or die('Database not available');
 				
-				$query = "SELECT * FROM Sprint2 WHERE methodologyname like '%{$input}%' or methodologydescription like '%{$input}%' ";
+				$query = "SELECT * FROM Sprint2 WHERE methodologyname like '%{$input}%' or methodologydescription like '%{$input}%'";
 				$results = mysqli_query($conn, $query);
 				
 				echo "<table width='200%' border ='1'>";
 				echo "<tr><th>methodology name</th><th>methodology description</th><th>method name</th><th>method description</th><th>bib ref</th><th>research level</th><th>Rating</th><th>cred reason</th><th>credrater</th><th>question</th><th>rd method</th><th>metrics</th><th>participants</th><th>benefit</th><th>result</th><th>method</th><th>context</th><th>con level</th><th>con reason</th><th>con rater</th></tr>";
 				
-						while($row = mysqli_fetch_assoc($results)) {
+				while($row = mysqli_fetch_assoc($results)) {
 					echo "<tr><td> {$row['methodologyname']}</td>";
 					echo "<td> {$row['methodologydescription']}</td>";
 					echo "<td> {$row['methodname']}</td>";
@@ -49,6 +49,9 @@
 				mysqli_free_result($results);
 				mysqli_close($conn);
 			?>
+		<br>
+		<a href="form.php">Return to Form</a>
+
 
 </body>
 </html>
