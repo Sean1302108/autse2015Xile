52123d00bba555ee0a97ed228dd890b67c5bcb0c
Sean.Y <sean_young_83@hotmail.com>
2015-05-29 10:29:42 +1200

    Create settings.php

---
+++ settings.php
@@ -0,0 +1,30 @@
+
+
+
+
+<?php
+
+
+
+	$host = "cmslamp14.aut.ac.nz"; 
+
+
+
+	$user = ""; // your user name 
+
+
+
+	$pswd = ""; // your password 
+
+
+
+	$dbnm = ""; // your database, same as username
+
+	
+
+
+
+
+
+
+?>
