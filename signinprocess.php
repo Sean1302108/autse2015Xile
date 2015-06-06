17619df42f8e889d61d167eef114e47702297e0f
Sean.Y <sean_young_83@hotmail.com>
2015-06-06 21:57:55 +1200

    Update signinprocess.php

--- signinprocess.php
+++ signinprocess.php
@@ -1,18 +1,66 @@
 <!DOCTYPE html>
-<html>
-	<head>
-		<title>Signed in</title>
+
+<html>	
+	<head>		
+		<title>Sign In</title>		
+		<link rel ="stylesheet" type ="text/css" href="style_1.css">	
 	</head>
 	<body> 
-	
-	<h1>SERLER</h1>
-		<?php
-			$user = $_POST["user"];
-			$password = $_POST["code"];
-	    		echo "Succesfully signed in";
-    		?>
-    		<a href="mainmenu.php">Return to Home Page</a>
-    </body>
-    </html>
+	<div id="wrap">
+	<div id ="header">
+		<h1>SERLER</h1>
+	</div>
+		<div id="right">
+		<font color="#FFE788"><h2> Sign in</h2></font>
+		<br>
 		
-		
+		<form action="postsignin.php" method="POST">	
+			
+			You have Successfully Signed in!
+			
+		</form>		
+	</div>
+	<div id="left">
+	<h2> Navigation</h2>
+	<ul>
+		<li>	
+			<a href="mainmenu.php">Home</a>
+		</li>
+		<li> 
+			<a href="form.php">Submit Article</a>
+		</li>
+		<li>
+			<a href="search.php">Search Article</a>
+		</li>
+		<li>
+			<a href="about.php">About</a>
+		</li>
+		<li>
+			<a href="signin.php">Sign in</a>
+		</li>
+		</ul>
+	<h2> Papers </h2>
+	<ul>
+		<li>
+			<a href="#"> Paper 1 </a>
+		</li>
+		<li>
+			<a href="#"> Paper 2 </a>
+		</li>
+		<li>
+			<a href="#"> Paper 3 </a>
+		</li>
+		<li>
+			<a href="#"> Paper 4 </a>
+		</li>
+		<li>
+			<a href="#"> Paper 5 </a>
+		</li>
+		</ul>
+	</div>
+	<div style ="clear: both;"> </div>
+	<div id ="footer">
+	SERLER Designed and produced by Team Xile
+	</div>
+	</body>		
+</html>		
