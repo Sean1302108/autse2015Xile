2936b11b7aa7f8de392f1f93d2bcc3f6bd2dc681
Sean.Y <sean_young_83@hotmail.com>
2015-06-06 21:53:33 +1200

    Update about.php

--- about.php
+++ about.php
@@ -2,21 +2,64 @@
 <html>
 <head>
 <title>About</title>
+		<link rel ="stylesheet" type ="text/css" href="style_1.css">	
 </head>
-<body>
-
-<h1>About SERLER</h1>
-
-	
-		<p>
-			SERLER is a Software engineering practice repository which hopes to <br/>
-			provide researchers and practitioners the ability to search and add <br/>
-			empirical based studies which can aid in the process of decision making <br/>
-			
-			<a href="mainmenu.php">Return to Home Page</a>
-			
-		</p>
-	?>
+<body> 
+	<div id="wrap">
+	<div id ="header">
+	<h1>SERLER</h1>
+	</div>
+		<div id="right">	
+	<p>
+	<font color="#FFE788"><h2> About </h2></font>
+	<br>
+		omg easter egg
+		<br><br>
+		Created By Sean Young, Ardee Sekhon and Antonie Chau
+	</p>
+	</div>
+	<div id="left">
+	<h2> Navigation</h2>
+	<ul>
+		<li>	
+			<a href="mainmenu.php">Home</a>
+		</li>
+		<li> 
+			<a href="form.php">Submit Article</a>
+		</li>
+		<li>
+			<a href="search.php">Search Article</a>
+		</li>
+		<li>
+			<a href="about.php">About</a>
+		</li>
+		<li>
+			<a href="signin.php">Sign in</a>
+		</li>
+		</ul>
+	<h2> Papers </h2>
+	<ul>
+		<li>
+			<a href="#"> Paper 1 </a>
+		</li>
+		<li>
+			<a href="#"> Paper 2 </a>
+		</li>
+		<li>
+			<a href="#"> Paper 3 </a>
+		</li>
+		<li>
+			<a href="#"> Paper 4 </a>
+		</li>
+		<li>
+			<a href="#"> Paper 5 </a>
+		</li>
+		</ul>
+	</div>
+	<div style ="clear: both;"> </div>
+	<div id ="footer">
+	SERLER Designed and produced by Team Xile
+	</div>
 
 </body>
 </html>
