7c677c53d1a07a170ecee750515c7eadf8b43ace
Sean.Y <sean_young_83@hotmail.com>
2015-06-06 21:57:30 +1200

    Update signin.php

--- signin.php
+++ signin.php
@@ -2,21 +2,27 @@
 
 <html>	
 	<head>		
-		<title>Please sign in</title>		
-	</head>	
-	<body>
+		<title>Sign In</title>		
+		<link rel ="stylesheet" type ="text/css" href="style_1.css">	
+	</head>
+	<body> 
+	<div id="wrap">
+	<div id ="header">
+	<h1>SERLER</h1>
+	</div>
+		<div id="right">
+		<font color="#FFE788"><h2> Sign in</h2></font>
+		<br>
 		
-		<h1>SERLER</h1>
-		
-		<form action="signinprocess.php" method="POST">	
+		<form action="postsignin.php" method="POST">	
 			
 			Username(required):			
-			<input type="text" name="user">		
+			<input type="text" name="status">		
 			<br>			
 			<br>
 			
 			Password (required):			
-			<input type="text" name="code" maxlength="5">
+			<input type="text" name="code">
 			<br>
 			<br>		
 			
@@ -25,9 +31,48 @@
 			<input type="reset" value="Reset"> 
 			
 		</form>		
-		
-		<br>		
-		<a href="mainmenu.php">Return to Home Page</a>
-		
+	</div>
+	<div id="left">
+	<h2> Navigation</h2>
+	<ul>
+		<li>	
+			<a href="mainmenu.php">Home</a>
+		</li>
+		<li> 
+			<a href="form.php">Submit Article</a>
+		</li>
+		<li>
+			<a href="search.php">Search Article</a>
+		</li>
+		<li>
+			<a href="about.php">About</a>
+		</li>
+		<li>
+			<a href="signin.php">Sign in</a>
+		</li>
+		</ul>
+	<h2> Papers </h2>
+	<ul>
+		<li>
+			<a href="#"> Paper 1 </a>
+		</li>
+		<li>
+			<a href="#"> Paper 2 </a>
+		</li>
+		<li>
+			<a href="#"> Paper 3 </a>
+		</li>
+		<li>
+			<a href="#"> Paper 4 </a>
+		</li>
+		<li>
+			<a href="#"> Paper 5 </a>
+		</li>
+		</ul>
+	</div>
+	<div style ="clear: both;"> </div>
+	<div id ="footer">
+	SERLER Designed and produced by Team Xile
+	</div>
 	</body>		
 </html>		
