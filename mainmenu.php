16370582d06a1ef99a89be9a83414e5df0d3afc3
Sean.Y <sean_young_83@hotmail.com>
2015-06-06 21:54:33 +1200

    Update mainmenu.php

--- mainmenu.php
+++ mainmenu.php
@@ -2,22 +2,71 @@
 <html>
 	<head>
 		<title>Main Menu</title>
+		<link rel ="stylesheet" type ="text/css" href="style_1.css">	
 	</head>
 	<body> 
-	
+		<div id="wrap">
+		<div id ="header">
 		<h1>SERLER</h1>
-		
-		<p>Welcome to SERLER: SERLER = SERL Evidence Repository. SERL = Software Engineering Research Lab at AUT: Select "Search Practices" below to see summaries of evidence-based software engineering studies.
-		<br>If you are a registered user and wish to add a new study and summary please select the "Post a new Practice" option below, or rate a study while searching through the practices.
-		</p>
-		
+		</div>
+			<div id="right">				
+			<font color="#FFE788"><h2> SERLER </h2></font>
+			<br>
 		<p>
-			<a href="postform.php">Post a new Practice</a>
-			<br><br>
-			<a href="searchform.php">Search Practice</a>
-			<br><br>
-			<a href="about.php">About this Website</a>
+		Welcome to SERLER: SERLER = SERL* Evidence Repository. <br>
+		SERLER aims to provide practitioners and researchers an easy way to research empirical studies in order to help make a decision 
+		when at a cross-roads for which practices to employ. Generally, SERLER will serve as an easy access repository in which members 
+		can submit, access, view and review papers.<br> <br>
+		Select "Search Practices" on the left to have a search through Empirical studies hosted by the repository. 
+		Otherwise if you are a registered user and wish to add a new study and summary please select the "Post a new Practice" option or rate a study while searching through the practices. 
+		If you have any questions or suggestions for improvement please e-mail us at ________@gmail.com 
 		</p>
+		
+		</div>
+	<div id="left">
+	<h2> Navigation</h2>
+	<ul>
+		<li>	
+			<a href="mainmenu.php">Home</a>
+		</li>
+		<li> 
+			<a href="form.php">Submit Article</a>
+		</li>
+		<li>
+			<a href="search.php">Search Article</a>
+		</li>
+		<li>
+			<a href="about.php">About</a>
+		</li>
+		<li>
+			<a href="signin.php">Sign in</a>
+		</li>
+		</ul>
+	<h2> Papers </h2>
+	<ul>
+		<li>
+			<a href="#"> Paper 1 </a>
+		</li>
+		<li>
+			<a href="#"> Paper 2 </a>
+		</li>
+		<li>
+			<a href="#"> Paper 3 </a>
+		</li>
+		<li>
+			<a href="#"> Paper 4 </a>
+		</li>
+		<li>
+			<a href="#"> Paper 5 </a>
+		</li>
+		</ul>
+	</div>
+	<div style ="clear: both;"> </div>
+	<div id ="footer">
+	SERLER Designed and produced by Team Xile
+	</div>
 		
 </body>
 </html>
+
+
