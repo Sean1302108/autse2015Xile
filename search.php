cde1328c8cc7384dfe708cd3a8ba13119826a52c
Sean.Y <sean_young_83@hotmail.com>
2015-06-06 21:56:26 +1200

    Update search.php

--- search.php
+++ search.php
@@ -2,22 +2,66 @@
 
 <html>
 	
-	<head>
-		<title>Search Status Form</title>
-	</head>
-	<body>
-
-			
-			<form action = "postsearch.php" method = "GET" >
-				Status: 
-				<input type="text" name="input">
-				<input type="submit" value="View Status">
-			</form>
-			
-			<p>
-				<a href="form.php">Return to form </a>
-			</p>
-			
+<head>
+	<title>Search Article</title>
+	<link rel ="stylesheet" type ="text/css" href="style_1.css">	
+</head>
+<body> 
+	<div id="wrap">
+	<div id ="header">
+	<h1>SERLER</h1>
+	</div>
+		<div id="right">
+			<font color="#FFE788"><h2> Search Article </h2></font>
+			<br>
+		<form action = "postsearch.php" method = "GET" >
+			Article Keyword: 
+			<input type="text" name="input">
+			<input type="submit" value="Search">
+		</form>
+		</div>
+			<div id="left">
+	<h2> Navigation</h2>
+	<ul>
+		<li>	
+			<a href="mainmenu.php">Home</a>
+		</li>
+		<li> 
+			<a href="form.php">Submit Article</a>
+		</li>
+		<li>
+			<a href="search.php">Search Article</a>
+		</li>
+		<li>
+			<a href="about.php">About</a>
+		</li>
+		<li>
+			<a href="signin.php">Sign in</a>
+		</li>
+		</ul>
+	<h2> Papers </h2>
+	<ul>
+		<li>
+			<a href="#"> Paper 1 </a>
+		</li>
+		<li>
+			<a href="#"> Paper 2 </a>
+		</li>
+		<li>
+			<a href="#"> Paper 3 </a>
+		</li>
+		<li>
+			<a href="#"> Paper 4 </a>
+		</li>
+		<li>
+			<a href="#"> Paper 5 </a>
+		</li>
+		</ul>
+	</div>
+	<div style ="clear: both;"> </div>
+	<div id ="footer">
+	SERLER Designed and produced by Team Xile
+	</div>
 			
 </body>
 </html>
